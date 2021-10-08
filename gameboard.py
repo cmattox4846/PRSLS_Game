8341b57a55950b270adeaf7fdb80e5771db3a18f
cmattox4846 <cmattox4846@gmail.com>
2021-10-08 14:47:16 -0400

    created display welcome message

---
+++ gameboard.py
@@ -0,0 +1,21 @@
+
+class GameBoard:
+    def __init__(self):
+        pass
+
+    def display_welcome(self):
+        print("Welcome to RPSLS (Rock, Paper, Scissors, Lizard, Spock)")
+        print("RPSLS is just like Rock Paper Scissors, each player has to choose any one variable Rock, Paper, Scissors, Lizard or Spock. Once the each player has chosen there vaiable, the choicces will be revealed and compared and winner will be decleared.\n The winneer will be decided by the chart below\n * Rock crushes Scissors  ---  Scissors cuts Paper --- Paper covers Rock --- Rock crushes Lizard --- Lizard poisons Spock \n Spock smashes Scissors --- Scissors decapitates Lizard --- Lizard eats Paper --- Paper disproves Spock --- Spock vaporizes Rock")
+        pass
+
+    def play_game(self):
+        pass
+
+    def player1_turn(self):
+        pass
+
+    def player2_turn(self):
+        pass
+
+    def display_winner(self):
+        pass
